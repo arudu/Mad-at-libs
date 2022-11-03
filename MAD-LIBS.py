3915e572b7a7aa3fb94b821dd9fca2d227d585ca
Paul Janne <94762681+arudu@users.noreply.github.com>
2022-11-03 01:24:10 -0400

    Add files via upload

---
+++ MAD-LIBS.py
@@ -0,0 +1,36 @@
+print('Hello')
+
+print('Welcome to a little game of MAD-LIBS!')
+
+message = input('Would you like to play?  ')
+
+if message == 'yes':
+    print('OK, well proceed')
+    
+else:
+    print('Buh-bye') 
+    
+noun1 = input('Please type a noun.  ')
+
+nounplural = input('Please type a noun plural.  ')
+
+noun2 = input('Please type another noun. ')
+
+place = input('Please type the name of a place. ')
+
+adjective = input('Please type an adjective. ')
+
+noun3 = input('Please type one more noun. ')
+
+print('Your mad-lib is....')
+
+print( ' Be kind to your ' + noun1 + ' -footed ' + nounplural + '.' )
+
+print( ' For a duck maybe somebody`s ' + noun2 + '.' )
+ 
+print( ' be kind to your ' + nounplural + ' in ' + place +'.')
+  
+print( ' where the weather is always ' + adjective + '.')
+
+print( ' you may think this is the ' + noun3 + ' ,well it is. ')
+
